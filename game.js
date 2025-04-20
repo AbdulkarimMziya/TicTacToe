f27585ce2b16ee9fa164034b9473af8f18269a17
AbdulkarimMziya <abdulmziya@gmail.com>
2025-04-20 15:54:03 -0700

    feat: add win and tie detection with game restart functionality

--- game.js
+++ game.js
@@ -53,6 +53,10 @@ const Game = (() => {
     }
 
     function handleClickEvent(event) {
+        if(gameOver) {
+            return;
+        }
+
         const index = parseInt(event.target.id.split("-")[1]);
 
         if(GameBoardModule.getGameBoard()[index] !== "") {
@@ -60,14 +64,31 @@ const Game = (() => {
         }
 
         GameBoardModule.update(index, players[currentPlayerIndex].symbol);
+
+        if(checkForWinner(GameBoardModule.getGameBoard(), players[currentPlayerIndex].symbol)){
+            gameOver = true;
+            alert(`${players[currentPlayerIndex].name} won!`);
+        } 
+        else if (checkForTie(GameBoardModule.getGameBoard())) {
+            gameOver = true;
+            alert("Its a Tie!!!");
+        }
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+    function restart() {
+        for(let i = 0; i < 9; i++) {
+            GameBoardModule.update(i,"");
+        }
+        GameBoardModule.render();
     }
     
     return {
         start,
-        handleClickEvent
+        restart,
+        handleClickEvent,
     }
-})();
+})();  
 
 
 function createPlayer(name, symbol) {
@@ -76,6 +97,37 @@ function createPlayer(name, symbol) {
         symbol
     }
 }
+
+function checkForWinner(board, playerSymbol) {
+    const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const [a, b, c] = winningCombinations[i];
+        if (board[a] &&
+            board[a] === playerSymbol &&
+            board[b] === playerSymbol &&
+            board[c] === playerSymbol
+        ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+const restartButton = document.querySelector('#restart-button');
+restartButton.addEventListener('click', () => {
+    Game.restart();
+});
 
 const startButton = document.querySelector('#start-button');
 startButton.addEventListener('click', ()=> {
